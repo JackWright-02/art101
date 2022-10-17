337db8d2e82ff6500a67f41430eee4e6ad4c8205
JackWright-02 <jarowrig@ucsc.edu>
2022-10-17 12:30:57 -0700

    lab 5 bby

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author:    Jack Wright
+ * Created:   13.10.2022
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make = "Jaguar";
+ model = "F-Type";
+ color = "Forest_Green";
+ year = 2013;
+
+ // calculate
+ age = 2023 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
